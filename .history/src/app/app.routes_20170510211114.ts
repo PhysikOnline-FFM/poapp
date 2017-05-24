dd828c3dc49c915f2e0bb521d431fbe05cbb2d7a
BastianKrones <bkrones.uni@gmail.com>
2017-05-24 21:03:09 +0200

    Reorganized the initial app with angular-material 2 framework

--- .history/src/app/app.routes_20170510211114.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from "@angular/router";
-import { TestComponent } from "./test/test.component";
-import{ PageNotFoundComponent } from "./pagenotfound/pagenotfound.component";
-
-export const appRoutes: Routes = [
-  { path: '', redirectTo: '/main', pathMatch: 'full' },
-  { path: 'main', component: TestComponent },
-  { path: '**', component: PageNotFoundComponent }
-]
