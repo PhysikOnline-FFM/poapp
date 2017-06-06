5146ebfc74fb4ec01a85c144bab9f1306ac70174
BastianKrones <bkrones.uni@gmail.com>
2017-06-07 00:31:05 +0200

    Last start-off for team-development

---
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagenotfound',
+  templateUrl: './pagenotfound.component.html',
+  styleUrls: ['./pagenotfound.component.scss']
+})
+export class PagenotfoundComponent implements OnInit {
+
+  pagenotfound = Math.floor(Math.random() * (4.9-1)+1);
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
